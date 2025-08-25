70fd2dbb8dffd3151d7ef32147cf928c0ee2e628
vanloc <tovanloc19@gmail.com>
2025-08-26 03:52:03 +0700

    fix saved database theme

--- routes/theme.routes.js
+++ routes/theme.routes.js
@@ -4,6 +4,6 @@ import { saveTheme } from '../controllers/theme.controller.js';
 const router = express.Router();
 
 // Route để lưu hoặc cập nhật theme theo uuid
-router.post('/theme', saveTheme);
+router.post('/', saveTheme);
 
 export default router;
