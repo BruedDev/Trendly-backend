28e474850e03ee9eadcdd5f7affe22803fd891b3
vanloc <tovanloc19@gmail.com>
2025-09-09 10:05:07 +0700

    feat: add payment routes and controller skeleton

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,6 +5,7 @@ import userRoutes from './user.routes.js';
 import productsRoutes from './product.routes.js';
 import cartRoutes from './cart.routes.js';
 import sanityWebhookRoutes from './sanity-webhook.routes.js';
+import payRoutes from './pay.routes.js';
 
 const router = express.Router();
 
@@ -12,6 +13,7 @@ router.use('/api/auth', authRoutes);
 router.use('/api/user', userRoutes);
 router.use('/api/products', productsRoutes);
 router.use('/api/cart', cartRoutes);
+router.use('/api/pay', payRoutes);
 router.use('/api/webhook', sanityWebhookRoutes);
 
 export default router;
