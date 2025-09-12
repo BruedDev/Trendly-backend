7eabf3f734b18527ef400eb749971707f80d9709
vanloc <tovanloc19@gmail.com>
2025-09-12 12:31:21 +0700

    feat: enhance checkout process with product details and implement remove item functionality

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -1,12 +1,15 @@
 import express from 'express';
-import { initiateCheckout } from '../controllers/payment.controller.js';
+import { initiateCheckout, removeItem } from '../controllers/payment.controller.js';
 import { authenticateToken } from '../middlewares/auth.middleware.js';
 import { checkUserInfo } from '../middlewares/checkUserInfo.middleware.js';
 
 const router = express.Router();
 // kiểm tra user và mã hóa phiên giao dịch
-router.post('/initiate-checkout', authenticateToken, checkUserInfo, initiateCheckout
-);
+
+// Xóa sản phẩm
+router.delete('/remove-item', authenticateToken, removeItem);
+
+router.post('/initiate-checkout', authenticateToken, checkUserInfo, initiateCheckout);
 
 export default router;
 
