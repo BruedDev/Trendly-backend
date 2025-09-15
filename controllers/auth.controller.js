cee663ff32a78d009e442674331452c4dfd1ad0d
vanloc <tovanloc19@gmail.com>
2025-09-15 09:52:23 +0700

    update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -29,7 +29,7 @@ export const login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'lax',
       maxAge: 365 * 24 * 60 * 60 * 1000
     });
     return res.json({
